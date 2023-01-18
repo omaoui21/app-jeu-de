8ee178374c4fe06db474a2045d87b2b4615f26e4
omaoui21 <zekoomaoui1999@gmail.com>
2023-01-18 14:07:05 +0100

    deploy my react app to github

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import JeuDe from './Components/JeuDe';
+function App() {
+  return (
+    <div className="App">
+      <JeuDe cache={6} />
+    </div>
+  );
+}
+
+export default App;
